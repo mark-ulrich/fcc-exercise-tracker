680043214b1751e0192713d42f07897614a56c7e
Glitch (glowing-conchoraptor) <none>
2019-11-15 03:58:16 +0000

    🎎🐴 Updated with Glitch

--- server.js
+++ server.js
@@ -1,32 +1,54 @@
+"use strict";
+
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
 const cors = require("cors");
 
+
+//////////////////////////////////////////
+//
+//    Database
+//
+
+// Connect
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MLAB_URI || "mongodb://localhost/exercise-track", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
+// Schemas
 const Schema = mongoose.Schema;
 const logSchema = new Schema({
   description: String,
   duration: Number,
-  date: { type: Date, default: Date.now }
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 const userSchema = new Schema({
   username: String,
-  count: {type: Number, default: 0 },
+  count: {
+    type: Number,
+    default: 0
+  },
   log: [logSchema]
 });
 
+// Models
 const User = mongoose.model("User", userSchema);
+
+
+
 
 app.use(cors());
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
 app.use(express.static("public"));
@@ -34,10 +56,16 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+
+
+
+
 // Add new user
 app.post("/api/exercise/new-user", (req, res) => {
   const username = req.body.username;
-  User.findOne({ username }, (err, user) => {
+  User.findOne({
+    username
+  }, (err, user) => {
     if (user) {
       res.status(400).send("username already taken");
       return;
@@ -48,20 +76,36 @@ app.post("/api/exercise/new-user", (req, res) => {
     user.save(err => {
       console.log(err);
     });
-    res.json({ username, _id: user.id });
+    res.json({
+      username,
+      _id: user.id
+    });
+  });
+});
+
+// List users
+app.get("/api/exercise/users", (req, res) => {
+  User.find({}, "username _id", (err, users) => {
+  res.json(users);
   });
 });
 
 // Add exercises
 app.post("/api/exercise/add", (req, res) => {
-  const { userId, description} = req.body;
+  const {
+    userId,
+    description
+  } = req.body;
   const duration = parseInt(req.body.duration, 10);
-  User.findOne({ _id: userId }, (err, user) => {
+
+  User.findOne({
+    _id: userId
+  }, (err, user) => {
     if (err) {
-      res.status(400).send('invalid user');
+      res.status(400).send("invalid user");
       return;
     }
-  
+
     // console.log(user);
     const date = req.body.date ? new Date(req.body.date) : new Date();
     user.log.push({
@@ -71,20 +115,72 @@ app.post("/api/exercise/add", (req, res) => {
     });
     user.count++;
     user.save((err) => console.log(err));
-    
-    const { username } = user;
-    res.json({ username, description, duration, _id: userId, date: date.toDateString() });
+
+    const {
+      username
+    } = user;
+    res.json({
+      username,
+      description,
+      duration,
+      _id: userId,
+      date: date.toDateString()
+    });
   });
 });
 
+
 // Get log
 app.get("/api/exercise/log", (req, res) => {
-  console.log("Not implemented");
+
+  const {
+    userId,
+    limit
+  } = req.query;
+  const from = req.query.from ? new Date(req.query.from) : new Date(0);
+  const to = req.query.to ? new Date(req.query.to) : new Date();
+
+  if (!userId) {
+    res.status(400).send(`Error: Please provide a user ID`);
+    return;
+  }
+
+  User.findById(userId, (err, user) => {
+    if (err) {
+      res.status(400).send(`Invalid user ID: ${userId}`);
+      return;
+    }
+
+    let log = user.log.filter(ent => ent.date >= from && ent.date <= to)
+                      .sort((a, b) => {
+                        if (a.date < b.date) return -1;
+                        if (a.date > b.date) return 1;
+                        else return 0;
+                      })
+                      .slice(0, limit)
+                      .map(ent => ({
+                        description: ent.description,
+                        duration: ent.duration,
+                        date: ent.date.toDateString()
+                      }));
+    let unfilteredLog = user.log;
+    res.json({
+      _id: user.id,
+      username: user.username,
+      count: user.count,
+      log,
+    });
+  });
+
 });
+
 
 // Not found middleware
 app.use((req, res, next) => {
-  return next({ status: 404, message: "not found" });
+  return next({
+    status: 404,
+    message: "not found"
+  });
 });
 
 // Error Handling middleware
